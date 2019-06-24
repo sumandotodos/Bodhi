ab545910da40d7eb412eba0b637bbb9a6d17451f
Emilio Pomares <suma@sumandotodos.es>
2019-06-24 08:17:58 +0200

    Updated gitignore to ignore node_modules

--- Backend/src/lib/config.js
+++ Backend/src/lib/config.js
@@ -5,7 +5,9 @@ const DevelopmentConfig = {
 	secretAccessKey: '<secretAccessKey>',
 	bucketName: 'myfoldraccel',	
 	region: 'eu-west-2',
-	psk: 'vQb9BpkcLGQWlmAild4B'
+	psk: 'vQb9BpkcLGQWlmAild4B',
+	dbhost: 'localhost',
+	dbport: '27017'
 }
 
 const ProductionConfig = {
@@ -13,7 +15,9 @@ const ProductionConfig = {
         secretAccessKey: '<secretAccessKey>',
         bucketName: 'myfoldraccel',
 	region: 'eu-west-2',
-	psk: 'KcbUv1heC80IGJyWlqlx'
+	psk: 'KcbUv1heC80IGJyWlqlx',
+	dbhost: 'localhost',
+	dbport: '27017'
 }
 
 function patchInEnv()
@@ -25,6 +29,14 @@ function patchInEnv()
 	if (process.env.AWSSECRETKEY != "")
 	{
 		config.secretAccessKey = process.env.AWSSECRETKEY
+	}
+	if (process.env.DBHOST != "")
+	{
+		config.dbhost = process.env.DBHOST
+	}
+	if (process.env.DBPORT != "") 
+	{
+		config.dbport = process.env.DBPORT
 	}
 }
 
